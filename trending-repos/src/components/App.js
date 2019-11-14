e42f32f31d1758f930466f566f68678b50355197
Outmane Baladi <outmanebaladi22@gmail.com>
2019-11-14 17:02:37 +0100

    create repository component

--- trending-repos/src/components/App.js
+++ trending-repos/src/components/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div>Hello!</div>
+    <div>
+    </div>
   );
 }
 
---
+++ trending-repos/src/components/Repository.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './Repository.css';
+
+function Repository({item}){
+    const {name, owner, description, stargazers_count, open_issues_count} = item;
+    return (
+      <div className="repo">
+          <div className="owner-avatar">
+              <img src={owner.avatar_url} />
+          </div>
+          <div className="repo-info">
+            <h1>{name}</h1>
+            <p>{description}</p>
+            <div className="repo-stats">
+                <p>Stars: {stargazers_count}</p>
+                <p>Issues: {open_issues_count}</p>
+                <p>Submitted 30 days ago by tensorflow</p>
+            </div>
+          </div>
+      </div>
+    );
+}
+
+export default Repository;
