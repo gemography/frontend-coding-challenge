192064dfef6fcd15aed86bc68dc00d7651148d95
Mina <mina.fawzy@sbdinc.com>
2022-10-02 21:00:21 +0200

    formate store

--- src/store/root/store.ts
+++ src/store/root/store.ts
@@ -1,30 +1,30 @@
-import React from "react";
-import { GithubRepoStore, IGithubRepoStore } from "../githubRepo/store";
+import React from 'react';
+import { GithubRepoStore, IGithubRepoStore } from '../githubRepo/store';
 
 export interface IRootStore {
-    githubRepoStore: IGithubRepoStore;
-    // other stores should come here 
+  githubRepoStore: IGithubRepoStore;
+  // other stores should come here
 }
 
 export function RootStore() {
-    const lazyStore = <S>(factory: (root: IRootStore) => S) => {
-      let _store: S | null = null;
-      return () => {
-        if (_store === null) {
-          _store = factory(store);
-        }
-        return _store;
-      };
-    };
-  
-    const _githubRepoStore = lazyStore(GithubRepoStore);
-
-    const store: IRootStore = {
-      get githubRepoStore() {
-        return _githubRepoStore();
+  const lazyStore = <S>(factory: (root: IRootStore) => S) => {
+    let _store: S | null = null;
+    return () => {
+      if (_store === null) {
+        _store = factory(store);
       }
+      return _store;
     };
-    return store;
-  }
-  
-  export const RootStoreContext = React.createContext(RootStore());
+  };
+
+  const _githubRepoStore = lazyStore(GithubRepoStore);
+
+  const store: IRootStore = {
+    get githubRepoStore() {
+      return _githubRepoStore();
+    },
+  };
+  return store;
+}
+
+export const RootStoreContext = React.createContext(RootStore());
