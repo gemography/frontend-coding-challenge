a9804e061ad83185feb755f622f66ca231ec22b6
Mina <mina.fawzy@sbdinc.com>
2022-10-02 21:01:09 +0200

    fix types to reflect the real data

--- src/store/githubRepo/types.ts
+++ src/store/githubRepo/types.ts
@@ -1,18 +1,29 @@
-
-export interface GithubRepoType {
-    owner:string;
-    name:string;
-    description:string;
-    has_issues:boolean;
-    open_issues_count:string;
-    stargazers_count:string;
-    created_at:string;
+export type GithubRepoResponseType = {
+  total_count: number;
+  items: GithubRepoType[];
+  incomplete_results: boolean;
+};
+type owner = {
+  avatar_url: string;
+};
+export type GithubRepoType = {
+  id: number;
+  name: string;
+  description: string;
+  has_issues: boolean;
+  open_issues_count: string;
+  stargazers_count: string;
+  created_at: string;
+  owner: owner;
+  html_url: string;
+};
+export enum OrderBy {
+  ASC = 'asc',
+  DESC = 'desc',
 }
-
 export interface GithubRepoInputOptions {
-    date:string;
-    sort: string;
-    order: string;
-    page: number|1;
-
-  }
+  date: string;
+  sort?: string;
+  order?: OrderBy;
+  page: number | 1;
+}
