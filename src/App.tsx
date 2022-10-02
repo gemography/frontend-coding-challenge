e3663ab52b09192d210b14e63469085a8309ef2a
Mina <mina.fawzy@sbdinc.com>
2022-10-02 21:03:06 +0200

    convert app, index to typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import MainLayout from './layout/MainContent';
+import RepoPage from './pages/RepoPage';
+
+function App() {
+  return (
+    <MainLayout>
+      <RepoPage />
+    </MainLayout>
+  );
+}
+
+export default App;
